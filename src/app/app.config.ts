d0a6ccc6f7c1a89fb113336f526e31256ff2c636
alapple <133106478+alapple@users.noreply.github.com>
2026-08-22 16:46:18 +0200

    feat: switch from axios to angular's native http ´client

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,21 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideOptimus } from '@openng/optimus-ui/config';
 import Aura from '@openng/optimus-ui-themes/aura';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { baseUrlInterceptor } from './interceptors/base-url.interceptor';
+import { API_BASE_URL } from './interceptors/base-url.token';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideOptimus({ theme: { preset: Aura } }),
+    provideHttpClient(
+      withInterceptors([baseUrlInterceptor])
+    ),
+    {
+      provide:API_BASE_URL,
+      useValue: 'http://localhost:8080',
+    }
   ],
 };
